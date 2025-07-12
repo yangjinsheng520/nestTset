6194da0c467f7d3472a9884a311e3f3827c1ee91
yangjinsheng520 <228643931@qq.com>
2025-07-13 01:31:13 +0900

    fix:项目初始化

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import elementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(elementPlus).use(router).mount("#app");
---
+++ src/server/request.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: "/api",
+  timeout: 5000,
+});
+
+export const addUser = (data: any) =>
+  service.post("/user", data).then((res) => res.data);
+
+export const getList = (data: any) =>
+  service.get("/user", { params: data }).then((res) => res.data);
+
+export const delUser = (data: any) =>
+  service.delete(`/user/${data.id}`).then((res) => res.data);
+export const updateUser = (data: any) =>
+  service.patch(`/user/${data.id}`, data).then((res) => res.data);
